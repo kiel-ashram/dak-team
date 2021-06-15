059476b68da2423750e6912fd692e80b0c60da5f
Julian <angela.julian@gmail.com>
2021-06-15 16:28:05 -0700

    added event listeners for LAT and LONG that return a number on click. Fixed list generation- both lists now contain data

--- assets/script.js
+++ assets/script.js
@@ -11,33 +11,48 @@ async function pokeHandler(num) {
     // deal with promise
     let pokeData = await response.json();
 
-    // create a list item with a div including sprite and pokemon ID + name
-    let pokeLi = document.createElement("li");
-    //create div
-    let pokeDiv = document.createElement("div");
-    //create img + set URL for img
-    let pokeImg = document.createElement("img");
-    pokeImg.src = pokeData.sprites.front_default;
-    //create pokemon label
-    let pokeTitle = document.createElement("h3");
-    let id = pokeData.id;
-    //format ID to show 3 places always
-    id = ("000" + id).slice(-3);
-    pokeTitle.textContent = id + ": " + pokeData.name;
-    pokeDiv.appendChild(pokeImg);
-    pokeDiv.appendChild(pokeTitle);
-    pokeLi.appendChild(pokeDiv);
-
     // add div to lists
     // TODO: only last list getting child. Why?
-    pokeLong.appendChild(pokeLi);
-    pokeLat.appendChild(pokeLi);
+    pokeLong.appendChild(createListElement(pokeData));
+    pokeLat.appendChild(createListElement(pokeData));
   }
 }
+
+var createListElement = function (pokeData) {
+  // create a list item with a div including sprite and pokemon ID + name
+  let pokeLi = document.createElement("li");
+  //create div
+  let pokeDiv = document.createElement("div");
+  //create img + set URL for img
+  let pokeImg = document.createElement("img");
+  pokeImg.src = pokeData.sprites.front_default;
+  //create pokemon label
+  let pokeTitle = document.createElement("h3");
+  let id = pokeData.id;
+  //format ID to show 3 places always
+  id = ("000" + id).slice(-3);
+  pokeTitle.textContent = id + ": " + pokeData.name;
+  pokeDiv.appendChild(pokeImg);
+  pokeDiv.appendChild(pokeTitle);
+  pokeLi.appendChild(pokeDiv);
+  pokeDiv.dexId = id;
+  pokeImg.dexId = id;
+  pokeLi.dexId = id;
+
+  return pokeLi;
+};
+
 // call for all 845 pokemon
 pokeHandler(845);
 
 // create onClick which passes lat and long data
+pokeLat.addEventListener("click", function (event) {
+  console.log(event.target.dexId);
+});
+
+pokeLong.addEventListener("click", function (event) {
+  console.log(event.target.dexId);
+});
 
 var latLong = function () {
   //define what parameters to pass
