ba8e1f345f7ff0c69070af48c30fe6dc7d873210
Julian <angela.julian@gmail.com>
2021-06-15 15:48:27 -0700

    added pokeHandler to make API call and create list of pokemon. TODOS: only one list populating. Need to populate both lists.

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,43 @@
-var lat = 0;
-var long = 0;
+var pokeLat = document.getElementById("poke-lat");
+var pokeLong = document.getElementById("poke-long");
 
-var getPokeList = function () {
-  return true; //prevents errors while function not defined
-  // generate a list of pokemon and display on page
-  //for each pokemon in list
-  //return pokedex number & name
+async function pokeHandler(num) {
+  // for 1-number passed
+  for (i = 1; i < num + 1; i++) {
+    // create URL
+    let url = "https://pokeapi.co/api/v2/pokemon/" + i + "/";
+    // request pokemod data
+    let response = await fetch(url);
+    // deal with promise
+    let pokeData = await response.json();
 
-  //OPTIONAL: paginated list? depending on runtime
-};
+    // create a list item with a div including sprite and pokemon ID + name
+    let pokeLi = document.createElement("li");
+    //create div
+    let pokeDiv = document.createElement("div");
+    //create img + set URL for img
+    let pokeImg = document.createElement("img");
+    pokeImg.src = pokeData.sprites.front_default;
+    //create pokemon label
+    let pokeTitle = document.createElement("h3");
+    let id = pokeData.id;
+    //format ID to show 3 places always
+    id = ("000" + id).slice(-3);
+    pokeTitle.textContent = id + ": " + pokeData.name;
+    pokeDiv.appendChild(pokeImg);
+    pokeDiv.appendChild(pokeTitle);
+    pokeLi.appendChild(pokeDiv);
 
-var choosePokemon = function () {
-  if (lat != 0) {
-    // clicked pokemon number assigned to lat
-  } else if (long != 0) {
-    //clicked pokemon number assigned to long
-  } else {
-    // prompt user to clear and try again
+    // add div to lists
+    // TODO: only last list getting child. Why?
+    pokeLong.appendChild(pokeLi);
+    pokeLat.appendChild(pokeLi);
   }
-};
+}
+// call for all 845 pokemon
+pokeHandler(845);
+
+// create onClick which passes lat and long data
 
 var latLong = function () {
   //define what parameters to pass
